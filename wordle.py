104dba3f16930a56044e63f3b9d610f8d2f53056
Aditeya <aditeya.baral@gmail.com>
2022-02-12 21:55:09 +0530

    Update solver for NYC Wordle

--- wordle.py
+++ wordle.py
@@ -13,12 +13,13 @@ options.add_argument('--ignore-ssl-errors=yes')
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--allow-running-insecure-content')
 
-url = "https://www.powerlanguage.co.uk/wordle/"
+url = "https://www.nytimes.com/games/wordle/"
 driver = webdriver.Chrome(options=options)
 driver.get(url)
 time.sleep(0.3)
 
-js_snippet = '''return window.localStorage.getItem("gameState")'''
-solution = json.loads(driver.execute_script(js_snippet))['solution'].upper()
+js_snippet = '''return window.localStorage'''
+local_storage = driver.execute_script(js_snippet)
+solution = json.loads(local_storage['nyt-wordle-state'])['solution'].upper()
 print(solution)
 driver.quit()
